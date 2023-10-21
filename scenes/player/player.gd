b0ed01de2d1032df8f0dbebcdbaa922ae8b4f11f
ButtercupSaiyan <ilikeroguelikes@gmail.com>
2023-10-21 19:35:31 -0400

    Added dialogue and sword ability

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,8 +2,23 @@ extends CharacterBody2D
 const MAX_SPEED = 200
 
 
+var enemy_in_range = false
+var is_talking: bool = false
+
 func _ready():
+
 	pass
+	
+
+func _physics_process(delta):
+	if enemy_in_range == true && is_talking == false:
+		is_talking = true
+		DialogueManager.show_example_dialogue_balloon(load("res://main.dialogue"), "start")
+	elif enemy_in_range == true && is_talking == true:
+		pass
+	else:
+		pass
+
 	
 func _process(delta):
 	var movement_vector = get_movement_vector()
@@ -17,3 +32,18 @@ func get_movement_vector():
 	var y_movement = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	return Vector2(x_movement, y_movement)
+
+
+
+func _on_detection_area_body_entered(body):
+	print(body.get_name())
+	if body.has_method("enemy"):
+		enemy_in_range = true
+		print("Enemy in range now")
+	
+
+func _on_detection_area_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_in_range = false
+		print("Enemy not in range now")
+
--- scriptswitcher.gd
+++ scriptswitcher.gd
@@ -1,24 +1,24 @@
 extends Control
-#
-#@export var scenes: Array[PackedScene]
-#
-#var scene_refs: Array[Node]
-#var index: int = 0
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	for scene in scenes:
-#		scene_refs.append(scene.instantiate())
-#
-#	replace_current_scene(scene_refs[index])
-#
-#
-#func replace_current_scene(scene: Node) -> void:
-#	for child in $SubViewportContainer/SubViewport.get_children():
-#		$SubViewportContainer/SubViewport.remove_child(child)
-#
-#	$SubViewportContainer/SubViewport.add_child(scene)
-#
-#func _on_button_pressed():
-#	index = (index + 1) % scene_refs.size()
-#	replace_current_scene(scene_refs[index])
+
+@export var scenes: Array[PackedScene]
+
+var scene_refs: Array[Node]
+var index: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for scene in scenes:
+		scene_refs.append(scene.instantiate())
+
+	replace_current_scene(scene_refs[index])
+
+
+func replace_current_scene(scene: Node) -> void:
+	for child in $SubViewportContainer/SubViewport.get_children():
+		$SubViewportContainer/SubViewport.remove_child(child)
+
+	$SubViewportContainer/SubViewport.add_child(scene)
+
+func _on_button_pressed():
+	index = (index + 1) % scene_refs.size()
+	replace_current_scene(scene_refs[index])
